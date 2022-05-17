b2d4832da124eb30aa7bc7505e8d76e7327ca03e
Daniel Kwon <danielkwon@Daniels-MBP.lan>
2022-05-16 18:37:09 -0700

    made formatting changes to scrapeBoxOffice.R

--- scrapeBoxOffice.R
+++ scrapeBoxOffice.R
@@ -1,41 +1,55 @@
-## author: andrew mashhadi
+#### ====================================================================== ####
+## Purpose: this script will pull data for top 2
+## Author: Andrew Mashadi
+## ========================================================================== ##
 
+#### ====================================================================== ####
+## Main
+## ========================================================================== ##
+
+## read Renviron file
+readRenviron("/Users/danielkwon/Repos/Stats-405-Data-Management/.Renviron")
+
+## libraries 
 library(rjson)
 
-xpath_main_data <- Sys.getenv("PATH_MY_MAIN_DATA")
-ximdb_api_key <- Sys.getenv("IMDB_API_KEY")
+## define parameters
+xpath_main_data   <- Sys.getenv("PATH_MY_MAIN_DATA")                            # Sys.getenv("PATH_MY_FINAL_PROJECT") # I used a different r envrion variable name so commenting that out
+ximdb_api_key     <- Sys.getenv("IMDB_API_KEY")
+xpath_scrape_imdb <- file.path(xpath_main_data, "imdb_data", "top_box_office")
 
-xpath_scrape_imdb <-
-file.path(xpath_main_data, "IMDb_data", "topBoxOffice")
-
+## creates folder defined by xpath_scrape_imdb if it does not already exist
 if(!dir.exists(xpath_scrape_imdb)) {
     dir.create(xpath_scrape_imdb, recursive=TRUE)
 }
 
-
-### EXTRACT THE TOP 200 BOX OFFICES FROM IMDB API
+## extracts the top 200 movies by box office from imdb api
 
 numTry <- 0
 xbool_keep_going <- TRUE
-while(xbool_keep_going & numTry < 5) {
-    
-    xxInfo <-
+while(xbool_keep_going & numTry < 5) {                                          # will try at most 4 times before error
+  
+  xxInfo <-
     try(
-    readLines( paste0("https://imdb-api.com/en/API/BoxOfficeAllTime/", ximdb_api_key) ),
-    silent=TRUE
-    )
-    
-    if("try-error" %in% class(xxInfo)) {
-        numTry <- numTry + 1
-        cat("Could not read movie list on attempt ", numTry, ". Retrying.")
-        Sys.sleep(1)
-    } else {
-        xfn <- paste0("BoxOfficeAllTime_Movies", ".json");
-        writeLines( xxInfo, file.path( xpath_scrape_imdb, xfn ) )
-        xbool_keep_going <- FALSE
-        cat("Got box office records\n")
-    }
+      readLines( paste0("https://imdb-api.com/en/API/BoxOfficeAllTime/", 
+                        ximdb_api_key) ),
+      silent=TRUE
+      )
+  
+  if("try-error" %in% class(xxInfo)) {
+    numTry <- numTry + 1
+    cat("Could not read movie list on attempt ", numTry, ". Retrying.")
+    Sys.sleep(1)
+  }
+  else {
+    xfn <- paste0("BoxOfficeAllTime_Movies", ".json");
+    writeLines( xxInfo, file.path( xpath_scrape_imdb, xfn ) )
+    xbool_keep_going <- FALSE
+    cat("Got box office records\n")
+  }
+  
 }
+
 
 
 

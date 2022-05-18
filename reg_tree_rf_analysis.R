ab6690983189a17c94d7f8086edec527e68926cd
Andrew Mashhadi <amiroandy7@gmail.com>
2022-05-18 16:32:45 -0700

    added reg tree analysis with new extd dataset

--- reg_tree_rf_analysis.R
+++ reg_tree_rf_analysis.R
@@ -32,6 +32,7 @@
 # 
 # imdb_boxOffice <- dbGetQuery(con, "SELECT * FROM imdb_boxOffice")
 # imdb_details <- dbGetQuery(con, "SELECT * FROM imdb_details")
+# imdb_details_extd <- dbGetQuery(con, "SELECT * FROM imdb_details_extd")
 # imdb_top250_movies <- dbGetQuery(con, "SELECT * FROM imdb_top250_movies")
 # imdb_top250_shows <- dbGetQuery(con, "SELECT * FROM imdb_top250_shows")
 # 
@@ -42,6 +43,7 @@
 #
 # write.csv(imdb_boxOffice, "C:\\Users\\amiro\\Desktop\\Statistics 405\\Week 5\\Final_Project_Brainstorming\\imdb_boxOffice.csv")
 # write.csv(imdb_details, "C:\\Users\\amiro\\Desktop\\Statistics 405\\Week 5\\Final_Project_Brainstorming\\imdb_details.csv")
+# write.csv(imdb_details_extd, "C:\\Users\\amiro\\Desktop\\Statistics 405\\Week 5\\Final_Project_Brainstorming\\imdb_details_extd.csv")
 # write.csv(imdb_top250_movies, "C:\\Users\\amiro\\Desktop\\Statistics 405\\Week 5\\Final_Project_Brainstorming\\imdb_top250_movies.csv")
 # write.csv(imdb_top250_shows, "C:\\Users\\amiro\\Desktop\\Statistics 405\\Week 5\\Final_Project_Brainstorming\\imdb_top250_shows.csv")
 # 
@@ -51,7 +53,7 @@ imdb_boxOffice <- read.csv("C:\\Users\\amiro\\Desktop\\Statistics 405\\Week 5\\F
 imdb_details <- read.csv("C:\\Users\\amiro\\Desktop\\Statistics 405\\Week 5\\Final_Project_Brainstorming\\imdb_details.csv")
 imdb_top250_movies <- read.csv("C:\\Users\\amiro\\Desktop\\Statistics 405\\Week 5\\Final_Project_Brainstorming\\imdb_top250_movies.csv")
 imdb_top250_shows <- read.csv("C:\\Users\\amiro\\Desktop\\Statistics 405\\Week 5\\Final_Project_Brainstorming\\imdb_top250_shows.csv")
-
+imdb_details_extd <- read.csv("C:\\Users\\amiro\\Desktop\\Statistics 405\\Week 5\\Final_Project_Brainstorming\\imdb_details_extd.csv")
 
 ########### USING PRUNED TREE #############
 
@@ -63,13 +65,13 @@ library(partykit)
 names(imdb_details)
 
 ## simple single tree
-tr <- rpart(imDbRating ~ budget+runtime+year+type+rating, data = imdb_details)
+tr <- rpart(imDbRating ~ budget+runtime+year+rating, data = imdb_details_extd)
 
 ## print plot to help choose cp
 plotcp(tr)
 
 ## prune the tree
-tr_2 <- prune(tr, cp = 0.023)
+tr_2 <- prune(tr, cp = 0.025)
 print(tr_2)
 
 
@@ -80,30 +82,30 @@ plot(as.party(tr_2), tp_args = list(id = FALSE))
 #######  USING RANDOM FOREST ########
 
 library(randomForest)
-imdb_rf = randomForest(imDbRating ~ budget+runtime+year+type+rating, data = imdb_details, mtry = 3, na.action = na.omit)
+imdb_rf = randomForest(imDbRating ~ budget+runtime+year+rating, data = imdb_details_extd, mtry = 3, na.action = na.omit)
 
 
 ### COMPARING ERRORs
 
-samp <- sample(673)
+samp <- sample(3271)
 rf_errors <- matrix(0, nrow=1, ncol=10)
 pt_errors <- matrix(0, nrow=1, ncol=10)
 for(k in 1:10){
-  from <- 1 + (k-1)*67
-  to <- 67*k # we will lose the last three observations
-  test <- na.omit(imdb_details[samp[from:to],])
-  train <- imdb_details[samp[-(from:to)],]
+  from <- 1 + (k-1)*327
+  to <- 327*k # we will lose the last three observations
+  test <- na.omit(imdb_details_extd[samp[from:to],])
+  train <- imdb_details_extd[samp[-(from:to)],]
   
   ## rf
-  imdb_rf <- randomForest(imDbRating ~ budget+runtime+year+type+rating, data = imdb_details, mtry = 3, na.action = na.omit)
+  imdb_rf <- randomForest(imDbRating ~ budget+runtime+year+rating, data = imdb_details_extd, mtry = 3, na.action = na.omit)
   
   ## pruned tree
-  tr <- rpart(imDbRating ~ budget+runtime+year+type+rating, data = imdb_details)
-  pt <- prune(tr, cp = 0.023)
+  tr <- rpart(imDbRating ~ budget+runtime+year+rating, data = imdb_details_extd)
+  pt <- prune(tr, cp = 0.025)
   
   ## calc errors
-  rf_errors[k] <- mean((imdb_details$imDbRating[test$X] - predict(imdb_rf, test))^2 ) 
-  pt_errors[k] <- mean((imdb_details$imDbRating[test$X] - predict(pt, test))^2 ) 
+  rf_errors[k] <- mean((imdb_details_extd$imDbRating[test$X] - predict(imdb_rf, test))^2 ) 
+  pt_errors[k] <- mean((imdb_details_extd$imDbRating[test$X] - predict(pt, test))^2 ) 
 }
 
 
@@ -114,8 +116,8 @@ mean(pt_errors)
 
 
 ###### PREDICTION ACCRUACY #####
-imdb_details_no_NA <- na.omit(imdb_details)
-imdb_rf = randomForest(imDbRating ~ budget+runtime+year+type+rating, data = imdb_details, mtry = 3)
+imdb_details_no_NA <- na.omit(imdb_details_extd)
+imdb_rf = randomForest(imDbRating ~ budget+runtime+year+rating, data = imdb_details_no_NA, mtry = 3)
 
 ## compare the predictions to the data 
 tr_comp <- data.frame(imDbRating=imdb_details_no_NA$imDbRating, predictedRating=predict(imdb_rf, imdb_details_no_NA))
